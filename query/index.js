76dd4e974ded21860ee18fcf463274e06fd7475b
Amit Barletz <amitbarletz@outlook.com>
2022-01-08 21:45:55 +0200

    added the query service which somehow disappeared from the previous commit

---
+++ query/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+const posts = {};
+
+const handleEvent = (type, data) => {
+    if (type === 'PostCreated') {
+        const {id, title} = data;
+        posts[id] = { id, title, comments: [] };
+    }
+
+    if (type === 'CommentCreated') {
+        const {id, content, postId, status} = data;
+        const post = posts[postId];
+        post?.comments.push({ id, content, status });
+    }
+
+    if (type === 'CommentUpdated') {
+        const {id, content, postId, status} = data;
+        const post = posts[postId];
+        const comment = post.comments.find(c => c.id === id);
+        comment.status = status;
+        comment.comment = content;
+    }
+}
+
+app.use(cors()); 
+
+app.get('/posts', (req, res) => {
+    res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+    const { type, data } = req.body;
+    handleEvent(type, data);
+
+    res.send({});
+});
+
+app.listen(4002, async () => {
+    console.log('listening on port 4002');
+
+    const res = await axios.get('http://localhost:4005/events');
+    for (let {type, data} of res.data) {
+        console.log('Proccessing event:', type);
+
+        handleEvent(type, data);
+    }
+});
